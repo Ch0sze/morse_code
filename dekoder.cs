9151432968fcb392496f2b703b3402306deba706
kebabdemon <95223277+kebabdemon@users.noreply.github.com>
2021-11-30 23:41:06 +0100

    dekoder
    dekoder

---
+++ dekoder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp3
+{
+    class dekoder
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Dekoduj("-|  ...-|  ---|  .---|  .|    --|  .-|  --|  .-|    .-..|  --|  .-|  ---|    -..-|  -..|    -..-|  -..|    -..-|  -..|    .----|  ..---|  ...--|"));
+            Console.WriteLine("Co chceš dekódovat?:");
+            string preloz = Console.ReadLine();
+            Console.WriteLine(Dekoduj(preloz));
+        }
+
+        public static string Dekoduj(string mkod) {
+            string[] mvety = mkod.Split("    ");
+            string prelozeno = "";
+
+            foreach (string mslovo in mvety)
+            {
+                string[] mpismena = mslovo.Split("  ");
+                string prelozenoslovo = "";
+
+                foreach (string mpismeno in mpismena)
+                {
+                    prelozenoslovo += morseovka.FirstOrDefault(x => x.Value == mpismeno).Key;
+                    // outputWord += MorseCode.Get(morseLetter); for codewars
+                }
+
+                prelozeno += (prelozeno == "") ? prelozenoslovo : " " + prelozenoslovo;
+
+            }
+            return prelozeno;
+        }
+
+        static Dictionary<char, string> morseovka = new Dictionary<char, string>(){
+
+             
+             {'a' , ".-|"},
+             {'b' , "-...|"},
+             {'c' , "-.-.|"},
+             {'d' , "-..|"},
+             {'e' , ".|"},
+             {'f' , "..-.|"},
+             {'g' , "--.|"},
+             {'h' , "....|"},
+             {'i' , "..|"},
+             {'j' , ".---|"},
+             {'k' , "-.-|"},
+             {'l' , ".-..|"},
+             {'m' , "--|"},
+             {'n' , "-.|"},
+             {'o' , "---|"},
+             {'p' , ".--.|"},
+             {'q' , "--.-|"},
+             {'r' , ".-.|"},
+             {'s' , "...|"},
+             {'t' , "-|"},
+             {'u' , "..-|"},
+             {'v' , "...-|"},
+             {'w' , ".--|"},
+             {'x' , "-..-|"},
+             {'y' , "-.--|"},
+             {'z' , "--..|"},
+             {'0' , "-----|"},
+             {'1' , ".----|"},
+             {'2' , "..---|"},
+             {'3' , "...--|"},
+             {'4' , "....-|"},
+             {'5' , ".....|"},
+             {'6' , "-....|"},
+             {'7' , "--...|"},
+             {'8' , "---..|"},
+             {'9' , "----.|"},
+        };
+    }
+}
